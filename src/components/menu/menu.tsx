248ce1e444b7fd4c85b72b50fe36a84e5f05ea17
Radu Stoica <radu.stoica1994@gmail.com>
2020-08-27 22:50:48 +0200

    added active menu item

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./menu.scss";
 
 import MenuItem from "../menuItem/menuItem";
 
+let menuItems = [
+  {
+    icon: "dashboard",
+    name: "Dashboard"
+  },
+  {
+    icon: "folder",
+    name: "Folders"
+  },
+  {
+    icon: "layers",
+    name: "Projects"
+  },
+  {
+    icon: "assignment",
+    name: "Tasks"
+  },
+  {
+    icon: "settings",
+    name: "Settings"
+  }
+];
+
 export default function Menu() {
+  let [active, setActiveMenu] = useState(0);
+
+  let onClick = (index: number) => {
+    setActiveMenu(index);
+  };
+
   return (
     <div className="menu-wrapper">
-      <MenuItem icon={"dashboard"} name={"Dashboard"} />
-      <MenuItem icon={"folder"} name={"Folders"} />
-      <MenuItem icon={"layers"} name={"Projects"} />
-      <MenuItem icon={"assignment"} name={"Tasks"} />
-      <MenuItem icon={"settings"} name={"Settings"} />
+      {menuItems.map((menuItem, index) => (
+        <MenuItem
+          icon={menuItem.icon}
+          name={menuItem.name}
+          isActive={active === index}
+          onClick={() => onClick(index)}
+        />
+      ))}
     </div>
   );
 }
--- src/components/menuItem/menuItem.tsx
+++ src/components/menuItem/menuItem.tsx
@@ -4,11 +4,16 @@ import "./menuItem.scss";
 interface MenuItemProps {
   icon: string;
   name: string;
+  onClick: Function;
+  isActive: boolean;
 }
 
 export default function MenuItem(props: MenuItemProps) {
   return (
-    <div className="menuItem-wrapper">
+    <div
+      className={`menuItem-wrapper ${props.isActive ? "active" : ""}`}
+      onClick={() => props.onClick()}
+    >
       <i className="material-icons-outlined">{props.icon}</i>
       <span className="name">{props.name}</span>
     </div>
