274cb3b8ef2578475f0be413788f362cde5f9cf2
Radu Stoica <radu.stoica1994@gmail.com>
2020-09-02 23:14:19 +0200

    added search bar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,5 +2,12 @@ import React from "react";
 import "./header.scss";
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <div className="header-wrapper">
+      <div className="searchBar">
+        <i className="material-icons">search</i>
+        <input className="searchBar-content" placeholder="Search"></input>
+      </div>
+    </div>
+  );
 }
