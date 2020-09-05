8e967946fc19c89cba1de7320d11da17aff1a035
Radu Stoica <radu.stoica1994@gmail.com>
2020-09-05 19:26:05 +0200

    designed basic task card

--- src/components/taskCard/taskCard.tsx
+++ src/components/taskCard/taskCard.tsx
@@ -5,12 +5,23 @@ import RoundCheckBox from "../roundCheckBox/roundCheckBox";
 
 interface TaskCardProps {
   id: string;
+  task?: string;
+  labelColor?: string;
+  labelText: string;
 }
 
 export default function TaskCard(props: TaskCardProps) {
   return (
     <div className="taskCard-wrapper">
-      <RoundCheckBox id={props.id} />
+      <div className="taskCard-top">
+        <RoundCheckBox id={props.id} />
+        <span className="task">{props.task}</span>
+      </div>
+      <div className="taskCard-bottom">
+        <label style={{ background: props.labelColor }}>
+          {props.labelText}
+        </label>
+      </div>
     </div>
   );
 }
--- src/components/taskWrapper/taskWrapper.tsx
+++ src/components/taskWrapper/taskWrapper.tsx
@@ -6,10 +6,30 @@ import TaskCard from "../taskCard/taskCard";
 export default function TaskWrapper() {
   return (
     <div className="task-wrapper">
-      <TaskCard id={"checkbox-1"} />
-      <TaskCard id={"checkbox-2"} />
-      <TaskCard id={"checkbox-3"} />
-      <TaskCard id={"checkbox-4"} />
+      <TaskCard
+        id={"checkbox-1"}
+        task={"Better quality favicon"}
+        labelColor={"#FF6161"}
+        labelText={"Design"}
+      />
+      <TaskCard
+        id={"checkbox-2"}
+        task={"Social accounts setup"}
+        labelColor={"#90C335"}
+        labelText={"Marketing"}
+      />
+      <TaskCard
+        id={"checkbox-3"}
+        task={"Upgrade plan page"}
+        labelColor={"#985BFF"}
+        labelText={"Development"}
+      />
+      <TaskCard
+        id={"checkbox-4"}
+        task={"Email templates"}
+        labelColor={"#FF6161"}
+        labelText={"Design"}
+      />
     </div>
   );
 }
